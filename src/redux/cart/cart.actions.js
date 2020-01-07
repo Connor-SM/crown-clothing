4b92cff84a308b715742ecfc52cd452c898f1804
Connor Milliken <cmilliken09@gmail.com>
2020-01-07 15:13:37 -0500

    add and remove item functionality with arrows applied in checkout, confirmation to delete window added in utils to remove entire item from checkout page

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -14,4 +14,9 @@ export const clearItemFromCart = item => ({
     payload: item
 });
 
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+});
+
 export default toggleCartHidden;
