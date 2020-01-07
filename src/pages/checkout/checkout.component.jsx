dcc47970120461f38a34b7f3f3cc703c76ec88d6
Connor Milliken <cmilliken09@gmail.com>
2020-01-07 12:21:24 -0500

    added empty cart message on checkout page, remove if causing issues down the road

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -9,37 +9,43 @@ import './checkout.styles.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
-        <div className='checkout-header'>
-            <div className='header-block'>
-                <span>Product</span>
+        { total ? 
+        <div>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
             </div>
 
-            <div className='header-block'>
-                <span>Description</span>
-            </div>
+            {
+                cartItems.map(cartItem =>
+                    <CheckoutItem cartItem={cartItem} key={cartItem.id} />
+                )
+            }
 
-            <div className='header-block'>
-                <span>Quantity</span>
-            </div>
-
-            <div className='header-block'>
-                <span>Price</span>
-            </div>
-
-            <div className='header-block'>
-                <span>Remove</span>
+            <div className='total'>
+                <span>TOTAL: ${total}</span>
             </div>
         </div>
-
-        {
-            cartItems.map(cartItem =>
-                <CheckoutItem cartItem={cartItem} key={cartItem.id} />
-            )
+        :
+        <span className='empty-message'>Your cart is empty</span>
         }
-
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
-        </div>
     </div>
 );
 
