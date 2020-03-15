320f484929861a137d7015acc215fdb25ca742c2
Connor Milliken <cmilliken09@gmail.com>
2020-03-15 15:54:47 -0400

    remove logger from prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)) );
 
