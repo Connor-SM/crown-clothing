1f0e5d1bc5f313a232ab36d9b1589cdf6bd1d9e8
Connor Milliken <cmilliken09@gmail.com>
2020-03-15 14:20:42 -0400

    updated search bar to use form input for better styles

--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { updateSearchTerm } from '../../redux/shop/shop.actions';
 import { selectSearchTerm } from '../../redux/shop/shop.selector';
+import FormInput from '../form-input/form-input.component';
 
 import './search-bar.styles.scss';
 
 const SearchBar = ({ searchTerm, updateSearchTerm }) => (
     <div className='search-bar'>
-        <input value={searchTerm} onChange={e => updateSearchTerm(e.target.value)} />
+        <FormInput
+            name='search' 
+            type='text' 
+            value={searchTerm}
+            handleChange={e => updateSearchTerm(e.target.value)}
+            label='search'
+        />
     </div>
 );
 
